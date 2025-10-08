bee3f01cd6820875b34327ec2f1815f33aa693da
“iyyann14” <iniiyyann@gmail.com>
2025-10-08 08:37:05 +0700

    Percobaan 1

---
+++ Jobsheet 5/CetakKRS09.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CetakKRS09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---Cetak KRS Siakad---");
+        System.out.println("Apakah UKT sudah lunas? (true/fales): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tanga DPA");
+        }
+    }
+}
