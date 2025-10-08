45fcc1a54ce944b3a41273ddb94b2f7c645fc75c
“iyyann14” <iniiyyann@gmail.com>
2025-10-08 10:49:10 +0700

    Tugas 3

--- Jobsheet 5/MaxMin09.java
+++ Jobsheet 5/MaxMin09.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 
 public class MaxMin09 {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
