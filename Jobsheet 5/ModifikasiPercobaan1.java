0aebc4de9b61016833d9efef20277aa0d7247920
“iyyann14” <iniiyyann@gmail.com>
2025-10-08 09:54:06 +0700

    Percobaan 1 Ternary

--- Jobsheet 5/ModifikasiPercobaan1.java
+++ Jobsheet 5/ModifikasiPercobaan1.java
@@ -8,11 +8,17 @@ public class ModifikasiPercobaan1 {
         System.out.println("Apakah UKT sudah lunas? (true/fales): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT Terverifikasi");
-            System.out.println("Silakan cetak KRS dan minta tanda tanga DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        System.out.println(
+            uktLunas 
+                ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA" 
+                : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu"
+        );
+
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT Terverifikasi");
+        //     System.out.println("Silakan cetak KRS dan minta tanda tanga DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        // }
     }
 }
