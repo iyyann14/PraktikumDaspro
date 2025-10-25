b5de98acf558c0fe8fe05e37e43070d4829bba36
“iyyann14” <iniiyyann@gmail.com>
2025-10-25 21:10:45 +0700

    Commit

---
+++ Jobsheet06/hmti7.java
@@ -0,0 +1,77 @@
+package Jobsheet06;
+
+import java.util.Scanner;
+
+public class hmti7 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // input jumlah tim
+        System.out.print("Masukkan Jumlah Tim: ");
+        int jumlahTim = sc.nextInt();
+
+        // input keterangan
+        System.out.print("Apakah Polinema memberikan dana bantuan? (ya/tidak): ");
+        String bantuan = sc.next();
+
+        double persenPolinema = 0;
+
+        // proses
+        if (bantuan.equalsIgnoreCase("ya")) {
+            System.out.print("Masukkan Persentase Bantuan (1-100): ");
+            int pilihan = sc.nextInt();
+
+            // switch case
+            switch (pilihan) {
+                case 10:
+                    persenPolinema = 10;
+                    break;
+                case 15:
+                    persenPolinema = 15;
+                    break;
+                case 25:
+                    persenPolinema = 25;
+                    break;
+                case 50:
+                    persenPolinema = 50;
+                    break;
+                case 90:
+                    persenPolinema = 90;
+                    break;
+                default:
+                    if (pilihan >= 1 && pilihan <= 90) {
+                        // Biaya tetap
+                        double publikasi = 300000;
+                        double dekorasi = 500000;
+                        double konsumsiPanitia = 500000;
+                        double hadiah = 4000000;
+                        double operasional = 500000;
+
+                        // Biaya berdasarkan jumlah tim
+                        int jumlahPeserta = jumlahTim * 3;
+                        double konsumsiPeserta = 25000 * jumlahPeserta;
+                        double honorJuri = 75000 * jumlahTim;
+
+                        // Total anggaran
+                        double total = publikasi + dekorasi + konsumsiPanitia + hadiah + operasional
+                                + konsumsiPeserta + honorJuri;
+
+                        // Hitung sumber dana
+                        double danaPolinema = total * (persenPolinema / 100);
+                        double pendaftaran = 50000 * jumlahTim;
+                        double sponsor = total - (danaPolinema + pendaftaran);
+
+                        // output
+                        System.out.println("Biaya Sponsor: " + sponsor);
+                        persenPolinema = pilihan;
+                    } else {
+                        System.out.println("Tidak Perlu Memberikan Bantuan");
+                        persenPolinema = 0;
+                    }
+            }
+        } else {
+            persenPolinema = 0;
+        }
+    }
+}
