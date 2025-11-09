76708dd73a10d1221c947f70986836567a162921
“iyyann14” <iniiyyann@gmail.com>
2025-11-09 08:12:12 +0700

    Commit

--- Jobsheet 8/ModifNilaiKel09.java
+++ Jobsheet 8/ModifNilaiKel09.java
@@ -7,24 +7,32 @@ public class ModifNilaiKel09 {
 
         double totalNilai;
         double rataNilai;
+        double rataTertinggi = 0;
+        int kelompokTertinggi = 0;
 
-        // Ganti WHILE dengan FOR
+        // Modif FOR
         for (int i = 1; i <= 6; i++) {
             System.out.println("Kelompok " + i);
-            totalNilai = 0;
+            totalNilai = 0; 
 
             for (int j = 1; j <= 5; j++) {
                 System.out.print("Masukkan nilai ke-" + j + ": ");
-                double nilai = sc.nextDouble();
-                totalNilai += nilai;
+                double nilai = sc.nextDouble(); 
+                totalNilai += nilai; 
             }
 
             rataNilai = totalNilai / 5;
-
             System.out.println("Rata-rata: " + rataNilai);
             System.out.println("---");
+
+            // Modif Rata-Rata Tinggi
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
         }
 
-        sc.close();
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok " 
+                           + kelompokTertinggi + " dengan rata-rata " + rataTertinggi);
     }
 }
