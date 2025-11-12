fa62d391aca9d9c4352510478090d5d1c16d50b2
“iyyann14” <iniiyyann@gmail.com>
2025-11-12 10:45:12 +0700

    Commit

--- Jobsheet 9/ArrayBilangan09.java
+++ Jobsheet 9/ArrayBilangan09.java
@@ -5,11 +5,10 @@ public class ArrayBilangan09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] bil = {5, 13, -7, 17};
+        int[] bil = { 5, 13, -7, 17 };
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
