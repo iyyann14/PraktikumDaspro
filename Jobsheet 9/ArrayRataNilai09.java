417499a82793d30242556528f134acde5ea4e96e
“iyyann14” <iniiyyann@gmail.com>
2025-11-12 11:18:25 +0700

    Commit

--- Jobsheet 9/ArrayRataNilai09.java
+++ Jobsheet 9/ArrayRataNilai09.java
@@ -6,18 +6,33 @@ public class ArrayRataNilai09 {
         Scanner sc = new Scanner(System.in);
 
         int[] nilaiMhs = new int[10];
+        int jumlahLulus = 0;
         double total = 0;
-        double rata2;
+        double rata2 = 0;
+        double tdklulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan Nilai Mahasiswa ke-"+(i + 1)+" : ");
+            System.out.print("Masukkan Nilai Mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                rata2 = total / nilaiMhs.length;
+            } else {
+                tdklulus = total / nilaiMhs.length;
+            }
+        }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus++;
+            }
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata - Rata Nilai = "+rata2);
+        System.out.println("Rata - Rata Nilai Lulus : " + rata2);
+        System.out.println("==========================");
+        System.out.println("Rata - Rata Nilai Tidak Lulus : " + tdklulus);
+        System.out.println("==========================");
+        System.out.println("Jumlah Mahasiswa yang Lulus : " + jumlahLulus);
     }
 }
