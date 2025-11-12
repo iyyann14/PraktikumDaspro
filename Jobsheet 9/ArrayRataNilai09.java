e456d75176302c2c9fbcc276436cff6758b21327
“iyyann14” <iniiyyann@gmail.com>
2025-11-12 11:42:24 +0700

    Commit

--- Jobsheet 9/ArrayRataNilai09.java
+++ Jobsheet 9/ArrayRataNilai09.java
@@ -7,6 +7,8 @@ public class ArrayRataNilai09 {
 
         int[] nilaiMhs = new int[10];
         int jumlahLulus = 0;
+        int nilaiTertinggi = 0;
+        int nilaiTerendah = 100;
         double total = 0;
         double rata2 = 0;
         double tdklulus = 0;
@@ -15,12 +17,23 @@ public class ArrayRataNilai09 {
             System.out.print("Masukkan Nilai Mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
+
+
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
             if (nilaiMhs[i] > 70) {
                 rata2 = total / nilaiMhs.length;
             } else {
                 tdklulus = total / nilaiMhs.length;
+            }
+        }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > nilaiTertinggi) {
+                nilaiTertinggi = nilaiMhs[i];
+            }
+
+            if (nilaiMhs[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiMhs[i];
             }
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
@@ -34,5 +47,9 @@ public class ArrayRataNilai09 {
         System.out.println("Rata - Rata Nilai Tidak Lulus : " + tdklulus);
         System.out.println("==========================");
         System.out.println("Jumlah Mahasiswa yang Lulus : " + jumlahLulus);
+        System.out.println("==========================");
+        System.out.println("Nilai Tertinggi : " + nilaiTertinggi);
+        System.out.println("==========================");
+        System.out.println("Nilai Terendah : " + nilaiTerendah);
     }
 }
