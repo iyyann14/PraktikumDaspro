d3a3cca61c4fe786f19d93c4dd8e7845a2e4dcb0
“iyyann14” <iniiyyann@gmail.com>
2025-11-15 12:17:25 +0700

    Commit

--- Jobsheet 9/SeacrhNilai09.java
+++ Jobsheet 9/SeacrhNilai09.java
@@ -4,18 +4,34 @@ public class SeacrhNilai09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        System.out.print("Masukkan Banyaknya Nilai yang akan Diinput: ");
+        int jumlah = sc.nextInt();
 
-        for (int i = 0; 1 < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+        int[] arrNilai = new int[jumlah];
+        for (int i = 0; i <arrNilai.length; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        int key = sc.nextInt();
+
+        int hasil = -1;
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (arrNilai[i] == key) {
                 hasil = i;
                 break;
             }
         }
+
         System.out.println();
-        System.out.println("Nilai "+key+" Ketemu di Indeks ke-"+hasil);
+
+        if (hasil != -1) {
+            System.out.println("Nilai" + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil+1));
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
+
         System.out.println();
     }
 }
