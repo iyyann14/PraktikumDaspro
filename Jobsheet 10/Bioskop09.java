f15eaf2b61c2aa2ae93af734431dd0994cfc9f15
“iyyann14” <iniiyyann@gmail.com>
2025-11-19 08:32:22 +0700

    Commit

--- Jobsheet 10/Bioskop09.java
+++ Jobsheet 10/Bioskop09.java
@@ -21,9 +21,9 @@ public class Bioskop09 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+        
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang Baris Ke-" + (i + 1) + ": " + penonton[i].length);
+        }
     }
 }
