bf37a5a6f124d6a06e9a112555f0f2a34a3cbaf4
“iyyann14” <iniiyyann@gmail.com>
2025-12-03 02:25:35 +0700

    Percobaan 4

---
+++ Jobsheet 12/PengunjungCafe09.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PengunjungCafe09 {
+
+    public static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung: ");
+        for (int i =  0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
