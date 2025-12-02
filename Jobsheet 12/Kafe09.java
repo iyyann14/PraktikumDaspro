c65026be5b9b37b29535409ebed56818ed892d73
“iyyann14” <iniiyyann@gmail.com>
2025-12-02 10:08:25 +0700

    Percobaan 1

---
+++ Jobsheet 12/Kafe09.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Kafe09 {
+
+    public static void main(String[] args) {
+        Menu();
+    }
+
+    public static void Menu() {
+        System.out.println("==== MENU KAFE RESTO ====");
+        System.out.println("1. Kopi Hitam - Rp 15.000");
+        System.out.println("2. Cappucino - Rp 20.000");
+        System.out.println("3. Latte - Rp 22.000");
+        System.out.println("4. Teh Tarik - 12.000");
+        System.out.println("5. Roti Bakar - Rp 10.000");
+        System.out.println("6. Mie Goreng - Rp 18.000");
+        System.out.println("==========================");
+        System.out.println("Silahkan Pilih Menu yang Anda Inginkan.");
+    }
+}
